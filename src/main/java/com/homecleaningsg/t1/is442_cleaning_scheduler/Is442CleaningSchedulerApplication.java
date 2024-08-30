ca2f6be996705c3676ce1f5104e77f7c2dc585b1
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-08-31 00:00:31 +0800

    ✨init: set up spring boiler plates

---
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/Is442CleaningSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.homecleaningsg.t1.is442_cleaning_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is442CleaningSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is442CleaningSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homecleaningsg/t1/is442_cleaning_scheduler/Is442CleaningSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homecleaningsg.t1.is442_cleaning_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is442CleaningSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
