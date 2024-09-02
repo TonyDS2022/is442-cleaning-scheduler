329baf255dbbb0dcf61dbe65b3abd4ed106b7e9a
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-09-02 17:16:56 +0800

    ✨feat: instantiate `location` table via jpa

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/location/Location.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/location/Location.java
@@ -1,24 +1,51 @@
 package com.homecleaningsg.t1.is442_cleaning_scheduler.location;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
 @EqualsAndHashCode
 @Getter
 @Setter
 @ToString
+@Entity
+@Table
 public class Location {
-    @JsonIgnore Long id;
-    @NonNull String address;
-    @NonNull String postalCode;
-    @JsonIgnore Double latitude;
-    @JsonIgnore Double Longitude;
-    @JsonIgnore Long regionId;
-    String regionName;
-    @JsonIgnore Long planningAreaId;
-    String planningAreaName;
-    @JsonIgnore Long subzoneId;
-    String subzoneName;
+    @Id
+    @SequenceGenerator(
+            name = "location_sequence",
+            sequenceName = "location_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @JsonIgnore
+    private Long id;
+
+    @NonNull
+    private String address;
+    @NonNull
+    private String postalCode;
+
+    @JsonIgnore
+    private Double latitude;
+    @JsonIgnore
+    private Double Longitude;
+
+    @JsonIgnore
+    private Long regionId;
+    private String regionName;
+
+    @JsonIgnore
+    private Long planningAreaId;
+    private String planningAreaName;
+
+    @JsonIgnore
+    private Long subzoneId;
+    private String subzoneName;
 }
