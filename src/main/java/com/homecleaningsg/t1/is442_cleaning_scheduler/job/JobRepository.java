6d6f8e12588c1b6719861c25d35831a740b6dce6
jenniferwxe11 <jennifer.wang@foodpanda.com>
2024-09-30 21:47:41 +0800

    revert last commit

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/job/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.homecleaningsg.t1.is442_cleaning_scheduler.job;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository<Job, Integer> {
-}
