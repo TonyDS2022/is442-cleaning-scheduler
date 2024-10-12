4b219c0c954cd24f26a011f0d3d05d0972325bad
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-10-12 23:47:25 +0800

    🐛fix: move worker instantiation to config constructor to ensure initializing order is respected

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/worker/WorkerConfig.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/worker/WorkerConfig.java
@@ -13,12 +13,14 @@ public class WorkerConfig implements CommandLineRunner{
 
     public WorkerConfig(WorkerRepository workerRepository) {
         this.workerRepository = workerRepository;
+
+        Worker ad1 = new Worker("Karthiga","karthiga","kar1243","karthigamagesh17@gmail.com", "99999999", "Hello, I am a sincere cleaner specialised in wooden floors", "8am-5pm");
+        Worker ad2 = new Worker("John","john","john1243","john@gmail.com", "99999999", "Cleaner with 20 years experience in cleaning bungalows/landed properties", "1pm-10pm");
+        this.workerRepository.saveAll(List.of(ad1, ad2));
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Worker ad1 = new Worker("Karthiga","karthiga","kar1243","karthigamagesh17@gmail.com", "99999999", "Hello, I am a sincere cleaner specialised in wooden floors", "8am-5pm");
-        Worker ad2 = new Worker("John","john","john1243","john@gmail.com", "99999999", "Cleaner with 20 years experience in cleaning bungalows/landed properties", "1pm-10pm");
-        workerRepository.saveAll(List.of(ad1, ad2));
+
     }
 }
