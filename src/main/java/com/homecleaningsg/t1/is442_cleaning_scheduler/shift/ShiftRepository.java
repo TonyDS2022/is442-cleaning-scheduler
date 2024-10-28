cea6182c73a56c5461af1d23ed1f6dfa3efaaa19
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-10-29 01:22:47 +0800

    🧹chore: remove unused imports

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/shift/ShiftRepository.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/shift/ShiftRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Collection;
 import java.util.List;
 
 @Repository
