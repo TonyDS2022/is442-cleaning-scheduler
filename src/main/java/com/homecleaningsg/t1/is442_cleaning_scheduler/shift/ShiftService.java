49bd92b4e4834987c08f4127d27b2a086ddc71d9
jenniferwxe11 <jenniferwxe@gmail.com>
2024-10-28 17:23:31 +0800

    Feat: get available workers for a shift
    check whether new shift overlaps with worker's current shifts and if its within worker's working hours

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/shift/ShiftService.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/shift/ShiftService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ShiftService {
@@ -86,5 +87,42 @@ public class ShiftService {
 
         shift.setWorker(worker);
         shiftRepository.save(shift);
+    }
+
+    public boolean shiftsTimeOverlap(Shift existingShift, Shift newShift){
+        // this encompasses all edge cases:
+        // 1. partial overlap
+        // 2. full overlap (new shift within existing shift/ existing shift within new shift)
+        // 3. touching boundaries (new shift ends exactly when the existing shift starts etc)
+        return !newShift.getSessionEndTime().isBefore(existingShift.getSessionStartTime())
+                && !newShift.getSessionStartTime().isAfter(existingShift.getSessionEndTime());
+    }
+
+    public boolean shiftIsWithinWorkingHours(Worker worker, Shift newShift){
+        return !newShift.getSessionStartTime().isBefore(worker.getStartWorkingHours())
+                && !newShift.getSessionEndTime().isAfter(worker.getEndWorkingHours());
+    }
+
+    public boolean isWorkerAvailable(Worker worker, Shift newShift){
+        List<Shift> workerShifts = this.getShiftsByDayAndWorker(newShift.getSessionStartDate(), worker.getWorkerId());
+        boolean hasConflict = workerShifts.stream()
+                .anyMatch(existingShift -> shiftsTimeOverlap(existingShift, newShift));
+
+        if(hasConflict){
+            return false;
+        }
+
+        return shiftIsWithinWorkingHours(worker,newShift);
+    }
+
+    public List<Worker> getAvailableWorkersForShift(Long shiftId) {
+        Shift newShift = shiftRepository.findById(shiftId)
+                .orElseThrow(() -> new IllegalArgumentException("Shift not found"));
+
+        List<Worker> allWorkers = workerRepository.findAll();
+
+        return allWorkers.stream()
+                .filter(worker -> isWorkerAvailable(worker, newShift))
+                .collect(Collectors.toList());
     }
 }
