81ffbc67cfb93e100f64c4ecd53051bc41bde6f0
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-10-29 01:19:07 +0800

    🐛fix: Change `cascadeType` to `DETACH`

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/worker/Worker.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/worker/Worker.java
@@ -60,7 +60,7 @@ public class Worker {
     @NonNull
     private LocalTime endWorkingHours;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.DETACH)
     @JoinColumn(name = "location_id")
     private Location location;
 
