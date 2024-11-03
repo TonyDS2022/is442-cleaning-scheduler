9dcad3a7fda92cb14369a6750dacd1c8f49c2e4d
CKChiangGit <88221395+CKChiangGit@users.noreply.github.com>
2024-11-03 20:56:21 +0800

    Refactor: Created Contract Creation endpoint with tweaks to LeaveApplication, shift and worker

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/contract/ContractController.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/contract/ContractController.java
@@ -1,10 +1,9 @@
 package com.homecleaningsg.t1.is442_cleaning_scheduler.contract;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,8 +24,32 @@ public class ContractController {
         return contractService.getContract();
     }
 
-    @GetMapping("/{contractId}/rate")
-    public Optional<Float> getRateByContractId(@PathVariable Long contractId) {
-        return contractService.getRateByContractId(contractId);
+    // add contract
+    // URL: POST /api/v0.1/contract
+    // {
+    //     "clientId": 1,
+    //         "contractStart": "2024-12-01T00:00:00.000+08:00",
+    //         "contractEnd": "2024-12-03T00:00:00.000+012:00",
+    //         "contractComment": "Contract Comment",
+    //         "ongoing": true,
+    //         "price": 100.0,
+    //         "workersBudgeted": 1,
+    //         "rooms": 3,
+    //         "frequency": "DAILY",
+    //         "sessionDurationMinutes": 180
+    // }
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Contract> createContract(@RequestBody Contract contract) {
+        Contract createdContract = contractService.createContract(contract);
+        return new ResponseEntity<>(createdContract, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public Contract updateContract(@PathVariable Long id, @RequestBody Contract contract) {
+        return contractService.updateContract(id, contract);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteContract(@PathVariable Long id) {
+        contractService.deleteContract(id);
     }
 }
