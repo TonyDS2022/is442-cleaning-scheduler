4cf6152846cbfba25d94fc1bdaabcd02f427f568
jenniferwxe11 <jenniferwxe@gmail.com>
2024-11-09 15:38:28 +0800

    Feat: created dto for admin statistics reporting
    - accordingly to yearly, monthly and weekly statistics

---
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/admin/MonthlyStatisticsDto.java
@@ -0,0 +1,26 @@
+package com.homecleaningsg.t1.is442_cleaning_scheduler.admin;
+
+import com.homecleaningsg.t1.is442_cleaning_scheduler.cleaningSession.SessionReportDto;
+import com.homecleaningsg.t1.is442_cleaning_scheduler.client.ClientReportDto;
+import com.homecleaningsg.t1.is442_cleaning_scheduler.contract.ContractReportDto;
+import com.homecleaningsg.t1.is442_cleaning_scheduler.shift.WorkerHoursDto;
+import com.homecleaningsg.t1.is442_cleaning_scheduler.worker.WorkerReportDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MonthlyStatisticsDto {
+    private LocalDate startOfMonth;
+    private LocalDate endOfMonth;
+    private List<WorkerHoursDto> workerMonthlyHours; //list of hours per worker
+    private ClientReportDto clientMonthlyReport;
+    private SessionReportDto sessionMonthlyReport;
+    private ContractReportDto contractMonthlyReport;
+    private WorkerReportDto workerMonthlyReport;
+}
