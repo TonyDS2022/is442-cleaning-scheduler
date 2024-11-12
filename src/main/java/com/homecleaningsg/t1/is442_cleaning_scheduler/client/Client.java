88b68b2e9c2ad7ca161ee104c40226fdec0d0e63
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-11-13 06:09:49 +0800

    🧹chore: seed client table with csv shard

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/client/Client.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/client/Client.java
@@ -51,6 +51,7 @@ public class Client {
     private LocalDate joinDate;
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference("client-clientSite")
     private List<ClientSite> clientSites = new ArrayList<>();
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
