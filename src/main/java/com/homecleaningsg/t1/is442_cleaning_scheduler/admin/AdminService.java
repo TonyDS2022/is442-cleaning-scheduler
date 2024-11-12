f800ab1395532eb89037d3321af171493741e814
jenniferwxe11 <jenniferwxe@gmail.com>
2024-11-13 00:20:53 +0800

    Feat: admin CRUD for rootadmin

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/admin/AdminService.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/admin/AdminService.java
@@ -3,6 +3,7 @@ package com.homecleaningsg.t1.is442_cleaning_scheduler.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -23,5 +24,25 @@ public class AdminService {
         return adminRepository.findByUsername(username).orElse(null);
     }
 
+    public Admin addAdmin(Admin admin){
+        admin.setJoinDate(LocalDate.now());
+        return adminRepository.save(admin);
+    }
+
+    public Admin updateAdmin(Long adminId, Admin updatedAdmin){
+        if(!adminRepository.existsById(adminId)){
+            throw new IllegalArgumentException("Admin not found");
+        }
+        updatedAdmin.setAdminId(adminId);
+        return adminRepository.save(updatedAdmin);
+    }
+
+    public Admin deactivateAdmin(Long adminId){
+        Admin admin = adminRepository.findById(adminId)
+                .orElseThrow(() -> new IllegalArgumentException("Admin not found"));
+        admin.setActive(false);
+        admin.setDeactivatedAt(LocalDate.now());
+        return adminRepository.save(admin);
+    }
 
 }
