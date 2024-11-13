e0eca8a7c8c08272aad2954a7ab7e71b3e6a4a10
Tony Zeng <tony.zeng.tianzhi@gmail.com>
2024-11-13 22:24:39 +0800

    🚧git: resolve conflict from #20

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/location/LocationRepository.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/location/LocationRepository.java
@@ -1,6 +1,7 @@
 package com.homecleaningsg.t1.is442_cleaning_scheduler.location;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.Optional;
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
     List<Location> findByLatitudeIsNullAndLongitudeIsNull();
-    Optional<Location> findByPostalCode(String postalCode);
+
+    @Query("SELECT l FROM Location l WHERE l.postalCode = :postalCode")
+    Location getLocationByPostalCode(String postalCode);
 }
