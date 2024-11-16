d7ac7b07ce452711d6b3a627c5ebaee69e1d8e05
jenniferwxe11 <jenniferwxe@gmail.com>
2024-11-16 22:48:07 +0800

    Fix: lombok getter setters for affectedShiftDto
    localhost:8080/api/v0.1/leave-applications/5/apply-annual-leave/?startDate=2024-10-12&endDate=2024-10-12 -> localhost:8080/api/v0.1/leave-applications/2/get-pending-leave-applications/
    
    should return 2 affected shifts

--- src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/leaveapplication/AffectedShiftDto.java
+++ src/main/java/com/homecleaningsg/t1/is442_cleaning_scheduler/leaveapplication/AffectedShiftDto.java
@@ -1,10 +1,15 @@
 package com.homecleaningsg.t1.is442_cleaning_scheduler.leaveapplication;
 
 import com.homecleaningsg.t1.is442_cleaning_scheduler.shift.Shift;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
+@Getter
+@Setter
 public class AffectedShiftDto {
     Long shiftId;
     LocalDate shiftStartDate;
